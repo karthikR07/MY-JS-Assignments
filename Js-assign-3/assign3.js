8bb4d0d25a4bdbb79235b694555a603051ee19c9
karthikR07 <you@example.com>
2022-07-22 11:43:17 +0530

    assign3 Done

--- Js-assign-3/assign3.js
+++ Js-assign-3/assign3.js
@@ -2,7 +2,7 @@ var license = prompt("You have driving license");
 var tired = prompt("you are tired");
 var sober = prompt("You are sober");
 if(license =="true"&& !(tired=="true")&& sober == "true"){
-    console.log("you can drive");
+    prompt("you can drive");
 }else{
-    console.log("you cannot drive");
+    prompt("you cannot drive");
 }
